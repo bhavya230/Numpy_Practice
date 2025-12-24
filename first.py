26c221a3c26e4c20781570b46a2bc0b57e3016c5
unknown <raghavbhavyasingh@gmail.com>
2025-12-24 13:26:37 +0530

    array indexing

--- first.py
+++ first.py
@@ -1,6 +1,6 @@
 import numpy as np
 list=[1,2,3]
-arr=np.array(list)
+arr=np.array(list,dtype=int)
 print(arr)
 print(arr.shape)
 list1=[2,3,4,5]
@@ -8,3 +8,7 @@ list2=[6,7,8,9]
 arr1=np.array([list1,list2])
 print(arr1)
 print(arr1.shape)
+
+# array indexing4
+print(arr[::2])
+print(arr1[0:1,1:3])
